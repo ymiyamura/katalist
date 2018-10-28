5b6f4776eec75b3c6b69ad1e2049d1a38a6c4714
hisomura <hisomura.noreply@gmail.com>
2018-10-28 17:36:59 +0900

    デフォルト値をいくつか修正

--- config/app.default.php
+++ config/app.default.php
@@ -255,14 +255,14 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_SCHEMA', ''),
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
             //'encoding' => 'utf8mb4',
-            'timezone' => 'UTC',
+            'timezone' => '+09:00',
             'flags' => [],
             'cacheMetadata' => true,
             'log' => false,
